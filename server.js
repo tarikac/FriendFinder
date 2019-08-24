102f6993d802c4da4c2a0e5b598601adf33a62aa
tarikac <tarika.common@gmail.com>
2019-08-23 23:15:44 -0500

    forgot to add routes in server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {

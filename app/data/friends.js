5a1e92de6a118c43db9797a5ff41e5c36ebf6302
tarikac <tarika.common@gmail.com>
2019-08-24 01:43:45 -0500

    friends objects

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+
+var friends = [{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,1,4,4,5,1,2,5,4,1
+      ]
+  },
+  {
+  "name":"Rika",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,1,2,4,2,1,2,5,4,2
+      ]
+    }
+]

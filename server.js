a00c7936eba309f2e4e0afe91a59699889b81739
tarikac <tarika.common@gmail.com>
2019-08-23 22:50:42 -0500

    home page set up to test heroku

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
-app.get("/home", function(req, res) {
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/home.html"))
 })
 

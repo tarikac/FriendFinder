787b7da8fe2969ab555a67a93dd56fe263fe1e9c
tarikac <tarika.common@gmail.com>
2019-08-23 23:24:26 -0500

    still was not calling routes properly.

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/data/routing/apiRoutes")(app);
+require("./app/data/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
